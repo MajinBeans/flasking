1a8622de00f33206cda821ad39a8b7b32bbc34b1
MajinBeans <dean.baeten@gmail.com>
2023-06-07 09:43:10 -0400

    Updated image ling in app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask
 app = Flask(__name__)
 images = [
-    "http://knowyourmeme.com/photos/181190-i-like-turtles"
+    "https://i.kym-cdn.com/photos/images/original/000/181/190/tumblr_lsdptrZkpi1qh6cr0o4_r1_500.gif"
 ]
 @app.route('/')
 def hello():
